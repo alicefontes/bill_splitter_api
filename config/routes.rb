5ba0319ab8a22303f02136dccf8062f73de544ac
Alice Fontes <alice.fontes@Mac-16.local>
2016-09-22 17:25:00 -0300

    opcao edit adicionada

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     get '/item', to: 'item#index'
     get '/item/:id', to: 'item#show'
-    # put '/item/:id', to: 'item#edit'
+    put '/item/:id', to: 'item#edit'
     post '/items', to: 'item#new'
     delete '/item/:id', to: 'item#delete'
 end
