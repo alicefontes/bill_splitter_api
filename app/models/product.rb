be2a7368cf6039488fbfe436137f22e2cce0c1bd
Alice Fontes <alice.fontes@Mac-16.local>
2016-10-13 16:21:34 -0300

    adicionado erro de divisao por zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,5 @@ class Product
   validates_length_of :name, maximum: 20
   validates_presence_of :price
   validates_presence_of :quantity
+  validates_numericality_of :number_of_people_sharing, :greater_than => 0
 end
